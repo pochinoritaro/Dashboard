4bcaf51f40fde99c1b737e9739af99544932935d
pochinoritaro <pochimoritaro@gmail.com>
2023-05-30 00:35:45 +0900

    add data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,22 @@
 import streamlit as st
-import pyvis
+import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
+from pyvis.network import Network
 import wikipedia as wiki
 
 wiki.set_lang("ja")
 text = wiki.summary("python", auto_suggest=False)
 page = wiki.page("python", auto_suggest=False)
+
+df = pd.read_csv("data/book1.csv")
+df = df.loc[df['weight'] > 10, :]
+
+
+G = nx.from_pandas_edgelist(df,)
+net = Network(notebook=True)
+net.from_nx(G)
+
 
 st.title(page.title)
 st.header(page.url)
